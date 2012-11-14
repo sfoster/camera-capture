8313f45bdd9923744d50db38e911f5b2d21823c5
sfoster <sam@sam-i-am.com>
2012-11-14 16:02:43 +0000

    Added open web apps -style manifest, some x-browser compat changes to video capture

--- app.js
+++ app.js
@@ -23,28 +23,37 @@ Author: Eric Bidelman (ericbidelman@chromium.org)
  *
  * @see http://developer.chrome.com/trunk/apps/manifest.html#permissions
  */
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia; 
 function getCamera() {
-  navigator.webkitGetUserMedia({audio: true, video: true}, function(stream) {
-    document.querySelector('video').src = webkitURL.createObjectURL(stream);
-  }, function(e) {
-    console.error(e);
-  });
+  if(navigator.getUserMedia) {
+    navigator.getUserMedia({audio: false, video: true}, function(stream) {
+      console.log("Got stream:", stream);
+      document.querySelector('video').src = ('webkitURL' in window) ? webkitURL.createObjectURL(stream) : stream;
+    }, function(e) {
+      console.error(e);
+    });
+  } else {
+    alert("No getUserMedia support in this browser");
+  }
 }
 
 /**
  * Click handler to init the camera grab
  */
 document.querySelector('button').addEventListener('click', function(e) {
-    getCamera();
+  console.log("button click, setting up camera");
+  getCamera();
 
-    var sourceImage = document.getElementById("source");
-    var destinationCanvas = document.getElementById("destination");
-    var context = destinationCanvas.getContext("2d");
+  var sourceImage = document.getElementById("source");
+  var destinationCanvas = document.getElementById("destination");
+  var context = destinationCanvas.getContext("2d");
 
-    var column = 0;
-    setInterval(function() {
-        context.drawImage(sourceImage, column, 0, 1, 480, column, 0, 1, 480)
-        column = (column + 1) % 640;
-    }, 10);
+  var column = 0;
+  console.log("setting up loop to render video data");
+  setInterval(function() {
+      context.drawImage(sourceImage, column, 0, 1, 480, column, 0, 1, 480)
+      column = (column + 1) % 640;
+  }, 10);
 
 });
